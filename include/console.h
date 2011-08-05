00d755f12b6878ed9f68b27abe8c84f00c007d64
wisen.wang.ah <wisen.wang.ah@gmail.com>
2011-08-05 13:06:17 +0800

    Symptom:[weboot] Add console subsystem directory
    Bug ID:
    Root Cause:
    Solution:
    Note:

---
+++ include/console.h
@@ -0,0 +1,8 @@
+#ifndef __CONSOLE_H____________
+#define __CONSOLE_H____________
+
+int console_read(void * buf, int len);
+int console_write(void * buf, int len);
+
+
+#endif
---
+++ subsys/console/console.c
@@ -0,0 +1,11 @@
+#include <console.h>
+
+int console_read(void * buf, int len)
+{
+
+}
+
+int console_write(void * buf, int len)
+{
+
+}
